268f502688bda9308a6a8db95fbffcef79ef2da1
1wnsgur1 <wick@rsquare.co.kr>
2024-09-17 21:03:34 +0900

    base commit

---
+++ api/complete.ts
@@ -0,0 +1,16 @@
+import { Copilot, registerCopilot } from "monacopilot";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+const copilot = new Copilot(process.env.OPENAI_API_KEY!, {
+  provider: "openai",
+  model: "gpt-4o-mini",
+});
+
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+  const completion = await copilot.complete({
+    body: req.body,
+  });
+  return res.json(completion);
+};
+
+export default handler;
