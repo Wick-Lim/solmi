ab0e298036c446e861bf4fe27c2a24b112453b7e
1wnsgur1 <wick@rsquare.co.kr>
2024-09-17 21:06:08 +0900

    base commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
 import Editor from "@monaco-editor/react";
-import { Copilot, registerCopilot } from "monacopilot";
-
-const copilot = new Copilot(import.meta.env.VITE_OPENAI_API_KEY, {
-  provider: "openai",
-  model: "gpt-4o-mini",
-});
 
 function App() {
   return (
@@ -12,10 +6,6 @@ function App() {
       height="90vh"
       defaultLanguage="javascript"
       defaultValue="// some comment"
-      onMount={(editor, monaco) => {
-        // registerCopilot(monaco, editor, {
-        // });
-      }}
     />
   );
 }
