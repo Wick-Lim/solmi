3381d567298ca8bc78be0f9d2fc6a7956f3efec9
1wnsgur1 <wick@rsquare.co.kr>
2024-09-18 11:39:00 +0900

    base commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import Editor from "@monaco-editor/react";
-import { Article, Folder, FolderOpen } from "@mui/icons-material";
-import { Box, ButtonBase, Divider, Tab, Tabs, Typography, useTheme } from "@mui/material";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { ChevronLeftOutlined, CloudOutlined, Extension, ExtensionOutlined, FileCopyOutlined, Folder, FolderOpen, MenuOutlined, SearchOutlined } from "@mui/icons-material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Divider, IconButton, InputAdornment, ListItemButton, ListItemIcon, ListItemText, Tab, Tabs, TextField, Typography } from "@mui/material";
 import { RichTreeView, TreeItem2, TreeViewBaseItem } from "@mui/x-tree-view";
 import { invoke } from "@tauri-apps/api/tauri";
 import { registerCopilot } from "monacopilot";
@@ -39,9 +40,30 @@ const MUI_X_PRODUCTS: TreeViewBaseItem[] = [
 ];
 
 function App() {
-  const theme = useTheme();
+  const [tab, setTab] = useState("file");
+  const [file, setFile] = useState("0");
 
-  const [tab, setTab] = useState("0");
+
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    if (monaco) {
+      // TypeScript 컴파일러 설정 적용 (tsconfig.json의 설정 반영)
+      monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+        jsx: monaco.languages.typescript.JsxEmit.React, // JSX 지원 설정
+        target: monaco.languages.typescript.ScriptTarget.ESNext, // 최신 ESNext 문법 사용
+        module: monaco.languages.typescript.ModuleKind.ESNext,  // ESNext 모듈 시스템 사용
+        allowJs: true,  // JavaScript 파일도 허용
+        strict: true,  // 엄격한 타입 검사 설정
+        noImplicitAny: true,  // any 타입 암시적 사용 금지
+      });
+
+      monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+        noSemanticValidation: false,
+        noSyntaxValidation: false
+      });
+    }
+  }, [monaco]);
 
   useEffect(() => {
     invoke("read_file", {
@@ -54,71 +76,122 @@ function App() {
   return (
     <Row flex="1">
       <Column width={300}>
-        <Row p='8px'>
-          <Typography component={ButtonBase} variant="subtitle2" fontWeight='bold'>
-            solmi
-          </Typography>
-        </Row>
-        <Divider />
-        <Tabs variant="fullWidth">
-          <Tab icon={<Article />} sx={{ minWidth: 0 }} />
-          <Tab icon={<Article />} sx={{ minWidth: 0 }} />
-          <Tab icon={<Article />} sx={{ minWidth: 0 }} />
-          <Tab icon={<Article />} sx={{ minWidth: 0 }} />
-          <Tab icon={<Article />} sx={{ minWidth: 0 }} />
-        </Tabs>
-        <Divider />
-        <RichTreeView
-          items={MUI_X_PRODUCTS}
-          slots={{ item: TreeItem2 }}
-          slotProps={{
-            item: {
-              slots: {
-                // @ts-ignore
-                label: Typography,
-                expandIcon: Folder,
-                collapseIcon: FolderOpen,
-              },
-              slotProps: {
-                // @ts-ignore
-                label: {
-                  variant: "caption",
-                },
-              },
-              sx: {
-                whiteSpace: "nowrap",
-                textOverflow: "ellipsis",
-              }
-            }
-          }}
-        />
+        <TabContext value={tab}>
+          <Row p='8px' alignItems='center'>
+            <Typography variant="subtitle2" fontWeight='bold'>
+              solmi
+            </Typography>
+            <Row flex="1" />
+            <IconButton size="small">
+              <MenuOutlined fontSize="small" />
+            </IconButton>
+          </Row>
+          <Divider />
+          <TabList variant="fullWidth" onChange={(_, tab) => setTab(tab)} TabIndicatorProps={{ hidden: true }}>
+            <Tab value="file" icon={<FileCopyOutlined />} sx={{ minWidth: 0 }} />
+            <Tab value="search" icon={<SearchOutlined />} sx={{ minWidth: 0 }} />
+            <Tab value="git" icon={<CloudOutlined />} sx={{ minWidth: 0 }} />
+            <Tab value="extension" icon={<ExtensionOutlined />} sx={{ minWidth: 0 }} />
+          </TabList>
+          <Divider />
+          <Column flex="1" position="relative">
+            <Column position="absolute" sx={{ inset: 0, overflowY: 'scroll' }}>
+              <Column height='fit-content'>
+                <TabPanel value="file" sx={{ p: '8px 0' }}>
+                  <RichTreeView
+                    items={MUI_X_PRODUCTS}
+                    slots={{ item: TreeItem2 }}
+                    slotProps={{
+                      item: {
+                        slots: {
+                          // @ts-ignore
+                          label: Typography,
+                          expandIcon: Folder,
+                          collapseIcon: FolderOpen,
+                        },
+                        slotProps: {
+                          // @ts-ignore
+                          label: {
+                            variant: "caption",
+                          },
+                        },
+                        sx: {
+                          whiteSpace: "nowrap",
+                          textOverflow: "ellipsis",
+                        }
+                      }
+                    }}
+                  />
+                </TabPanel>
+                <TabPanel value="search" sx={{ p: '8px 0' }}>
+                  <TextField size="small" variant="outlined" slotProps={{
+                    input: {
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton size="small">
+                            <SearchOutlined />
+                          </IconButton>
+                        </InputAdornment>
+                      )
+                    }
+                  }} fullWidth sx={{ px: '8px' }} />
+                </TabPanel>
+                <TabPanel value="git" sx={{ p: '8px 0' }}>
+                </TabPanel>
+                <TabPanel value="extension" sx={{ p: '8px 0' }}>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <Extension />
+                    </ListItemIcon>
+                    <ListItemText primary="Copilot" secondary="code development assistant" />
+                  </ListItemButton>
+                </TabPanel>
+              </Column>
+            </Column>
+          </Column>
+        </TabContext>
       </Column>
       <Divider orientation="vertical" flexItem sx={{ borderWidth: 2 }} />
       <Column flex="1">
-        <Tabs value={tab} onChange={(_, tab) => setTab(tab)}>
-          <Tab value="0" label="file231231.tsx" />
-          <Tab value="1" label="file231231.tsx" />
-          <Tab value="2" label="file231231.tsx" />
-        </Tabs>
-        <Box
-          flex="1"
-          position="relative"
-          py="8px"
-          bgcolor={theme.palette.grey[900]}
-        >
-          <Editor
-            height="100%"
-            theme="vs-dark"
-            defaultLanguage="javascript"
-            defaultValue=""
-            onMount={(editor, monaco) => {
-              registerCopilot(monaco, editor, {
-                endpoint: "https://solmi.vercel.app/api/complete",
-                language: "javascript",
-              });
-            }}
-          />
-        </Box>
+        <Row alignItems="flex-end">
+          <IconButton size="small">
+            <ChevronLeftOutlined fontSize="small" />
+          </IconButton>
+          <Tabs value={file} onChange={(_, tab) => setFile(tab)}>
+            <Tab value="0" label="file231231.tsx" />
+            <Tab value="1" label="file231231.tsx" />
+            <Tab value="2" label="file231231.tsx" />
+          </Tabs>
+        </Row>
+        <Column flex="1" position="relative">
+          <Column position="absolute" py="8px" bgcolor="#1e1e1e" sx={{ inset: 0 }}>
+            <Editor
+              theme="vs-dark"
+              path={'sample.tsx'}
+              defaultLanguage="typescript"
+              value={`
+class Foo {
+    private value = 42;
+    render() {
+        return <div>
+            <div>
+                Hello World
+            </div>
+            <div>The value is {this.value}</div>
+        </div>
+    }
+}
+`}
+              onMount={(editor, monaco) => {
+                // registerCopilot(monaco, editor, {
+                //   endpoint: "https://solmi.vercel.app/api/complete",
+                //   language: "typescript",
+                // });
+              }}
+              onChange={(value, e) => console.log(value, e)}
+            />
+          </Column>
+        </Column>
       </Column>
     </Row>
   );
